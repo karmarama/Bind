5d72d753fad31ee33266dd38fad1e410f5f69f0f
Tim Searle <tim.searle@karmarama.com>
2019-11-18 12:05:14 +0000

    Adding commentst

--- Sources/Bind/Relay.swift
+++ Sources/Bind/Relay.swift
@@ -1,3 +1,5 @@
+/// A concrete subclass of Output that purely emits values as it receives them but
+/// does not replay them on future subscribtion events
 public final class Relay<Value>: Output<Value> {
     public override func update(withValue value: Value) {
         super.update(withValue: value)
